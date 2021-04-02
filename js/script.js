488398ae901969aad29148e06ab002f1b64f2c75
ntipeter <71006073+nti-peter@users.noreply.github.com>
2021-04-02 15:20:49 +0200

    save 7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navLinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+    document.querySelector('.navLinks').classList.toggle('navActive')
+    document.querySelector('.burger').classList.toggle('toggle')
+}
+
+burger.addEventListener('click', toggleMeny)
